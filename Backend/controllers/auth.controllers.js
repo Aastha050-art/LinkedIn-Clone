60c345044b2791527a96ef90110c2b88e690a793
Aastha050-art <aasthamalviya510@gmail.com>
2025-10-14 14:36:22 +0530

    Update auth.controllers.js

--- Backend/controllers/auth.controllers.js
+++ Backend/controllers/auth.controllers.js
@@ -29,7 +29,7 @@ export const signUp=async(req,res)=>{
          httpOnly:true,
         maxAge:7*24*60*60*1000,
         sameSite:"none",
-        secure: true",
+        secure: true,
          })
 
         return res.status(201).json(user)
