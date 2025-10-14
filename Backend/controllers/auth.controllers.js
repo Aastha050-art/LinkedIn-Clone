6388e6bf00ad75c161baf09c67821dcd44e02106
Aastha050-art <aasthamalviya510@gmail.com>
2025-10-14 15:00:07 +0530

    Update auth.controllers.js

--- Backend/controllers/auth.controllers.js
+++ Backend/controllers/auth.controllers.js
@@ -30,6 +30,7 @@ export const signUp=async(req,res)=>{
         maxAge:7*24*60*60*1000,
         sameSite:"none",
         secure: true,
+        path:"/"
          })
 
         return res.status(201).json(user)
@@ -60,8 +61,8 @@ export const login =async(req,res)=>{
         res.cookie("token",token,{
         httpOnly:true,
         maxAge:7*24*60*60*1000,
-        sameSite:"strict",
-        secure: process.env.NODE_ENV==="production"
+        sameSite:"none",
+        secure:true,
          })
          
        
